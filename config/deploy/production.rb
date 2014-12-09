063576982cf8ef8e6b8642049a4199b7a023ad4f
Ashish Gupta <ashishgupta13@gmail.com>
2014-12-09 12:16:41 -0500

    More capistrano settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,9 +4,9 @@
 # is considered to be the first unless any hosts have the primary
 # property set.  Don't declare `role :all`, it's a meta role.
 
-role :app, %w{deploy@128.199.235.38}
-role :web, %w{deploy@128.199.235.38}
-role :db,  %w{deploy@128.199.235.38}
+role :app, %w{deployer@128.199.235.38}
+role :web, %w{deployer@128.199.235.38}
+role :db,  %w{deployer@128.199.235.38}
 
 
 # Extended Server Syntax
@@ -15,7 +15,7 @@ role :db,  %w{deploy@128.199.235.38}
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '128.199.235.38', user: 'deploy', roles: %w{web app}
+server '128.199.235.38', user: 'deployer', roles: %w{web app}
 
 
 # Custom SSH Options
