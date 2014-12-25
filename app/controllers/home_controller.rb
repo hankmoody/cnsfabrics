9d001d12dfcf006c65df0421891c6bb8e4f6f9d5
Ashish Gupta <ashishgupta13@gmail.com>
2014-12-25 16:19:07 -0500

    Debugging production

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,8 @@ class HomeController < ApplicationController
   before_filter :authenticate_user!, except: [:index]
 
   def index
-    @fabrics = Fabric.order(created_as: :desc).page(params[:page])
+    # @fabrics = Fabric.order(created_as: :desc).page(params[:page])
+    @fabrics = Fabric.all
   end
 
   def admin
