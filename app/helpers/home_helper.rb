390cf0460c411e95a274312da71217dd33d819c2
Ashish Gupta <ashishgupta13@gmail.com>
2016-04-16 16:57:56 -0400

    adding meta tags and making whatsapp button work

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,9 @@
 module HomeHelper
+  def meta_tag_image
+    if @fabrics.empty? || @fabrics.first.image.nil?
+      'http://fakeimg.pl/300x250/'
+    else
+      @fabrics.first.image.url(:thumbnail)
+    end
+  end
 end
